bed311f934cabd3b066a359627edf200ec6ecd72
chuyuwu09 <yukina01070921@gmail.com>
2023-03-16 08:28:18 +0800

    add if not in part

--- driving.py
+++ driving.py
@@ -1,17 +1,18 @@
 country = input('請問你是哪國人: ')
-age = input('你輸入年齡: ')
-age = int(age)
-if country == '台灣':
-    if age  >= 18:
-        print('你可以考駕照')
-    else:
-        print('你還不能考駕照')
-elif country == '美國':
-    if age  >= 16:
-        print('你可以考駕照')
-    else:
-        print('你還不能考駕照') 
 
+if country not in ['台灣', '美國']:
+    print('你只能輸入 台灣/美國')
 else:
-    print("只能輸入 台灣/美國")
+    age = input('你輸入年齡: ')
+    age = int(age)
     
+    if country == '台灣':
+        if age >= 18:
+            print('你可以考駕照')
+        else:
+            print('你還不能考駕照')
+    elif country == '美國':
+        if age >= 16:
+            print('你可以考駕照')
+        else:
+            print('你還不能考駕照')
